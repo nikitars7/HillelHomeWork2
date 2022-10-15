6566a6d2e4516d99a083f45c104087316eff0122
Nikita <nikita_rs7@ukr.net>
2022-10-15 22:51:41 +0300

    secondhomework

---
+++ home.js
@@ -0,0 +1,19 @@
+
+
+const num1 = prompt("Введите первое число:", "");
+const num2 = prompt("Введите второе число:", "");
+
+let summ = +num1 + +num2;
+
+if (summ == '') {
+   alert('The Summ of nothing =))')
+
+}
+else if (isNaN(summ)) {
+
+   alert('U can`t summ letters and words or use two or more numbers in 1 form')
+
+}
+else {
+   alert(`Сумма двух чисел = ${summ}`);
+}
